33260eb0c7432fc98025d8f895e12d7da4360b55
Mykyta Kasianenko <mka@empat.tech>
2024-09-25 18:26:49 +0300

    feat: add socketio

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ async def lifespan(app_: fastapi.FastAPI):
 
     models.SQLModel.metadata.create_all(deps.sync_engine)
 
+    import sio
+    import socketio
+
+    sio_app = socketio.ASGIApp(socketio_server=sio.server, other_asgi_app=app_)
+    app.add_route("/socket.io/", route=sio_app, methods=["GET", "POST"])
+    app.add_websocket_route("/socket.io/", sio_app)
+
+    # @app.get("/hello")
+    # async def root():
+    #     await sio.server.emit("response", "hello everyone")
+    #     return {"message": "hello"}
+
     yield
 
 
---
+++ sio/__init__.py
@@ -0,0 +1,8 @@
+import socketio
+
+mgr = socketio.AsyncRedisManager(url="redis://localhost:6379/0")
+server = socketio.AsyncServer(
+    async_mode="asgi", cors_allowed_origins="*", client_manager=mgr
+)
+
+__all__ = ["server"]
