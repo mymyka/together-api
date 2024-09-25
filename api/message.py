4cf7fa3e548d3678ab6afa878f8085c11eb3619c
Mykyta Kasianenko <mka@empat.tech>
2024-09-25 23:52:06 +0300

    feat: moved to connect

--- api/message.py
+++ api/message.py
@@ -1,18 +1,23 @@
+import typing
+
 import sio
-import typing as t
 import socketio.exceptions as sio_exc
 import deps
 import sqlalchemy.orm as orm
+import sqlalchemy.ext.asyncio as async_orm
 import sqlmodel
 import models
 
 
-async def begin_chat(sid, user_id: int):
-    session = deps.SessionFactory()
+async def get_user(
+    user_id: int,
+    *,
+    session: async_orm.AsyncSession,
+) -> models.User:
     stmt = (
         sqlmodel.select(models.User)
         .where(models.User.id == user_id)
-        .options(orm.selectinload(models.User.channels))
+        .options(orm.selectinload("*"))
     )
     result = await session.execute(stmt)
     user = result.scalars().one_or_none()
@@ -20,10 +25,23 @@ async def begin_chat(sid, user_id: int):
     if not user:
         raise sio_exc.ConnectionRefusedError("User not found")
 
-    for channel in user.channels:
-        await sio.server.enter_room(sid, channel.id)
+    return user
 
-    await session.close()
+
+async def enter_in_chats(
+    sid: str,
+    user: models.User,
+) -> None:
+    for channel in user.channels:
+        await sio.server.enter_room(sid, room=channel.id)
+
+
+async def exit_from_chats(
+    sid: str,
+    user: models.User,
+) -> None:
+    for channel in user.channels:
+        await sio.server.leave_room(sid, room=channel.id)
 
 
 @sio.server.on("connect")
@@ -31,22 +49,27 @@ async def connect(sid, environ):
     user_id = environ.get("HTTP_X_USER_ID")
     if user_id is None:
         raise sio_exc.ConnectionRefusedError("user_id is required")
-    user_id = int(user_id)
 
-    await sio.server.save_session(sid, {"user_id": user_id})
-    print(f"User {user_id} connected")
-
-    await begin_chat(sid, user_id)
+    session = deps.SessionFactory()
+    user = await get_user(int(user_id), session=session)
+    await sio.server.save_session(sid, {"user_id": user.id})
+    await enter_in_chats(sid, user)
+    await session.close()
 
 
 @sio.server.on("disconnect")
 async def disconnect(sid):
-    session = await sio.server.get_session(sid)
-    print(f'User {session.get('user_id')} disconnected')
+    session = deps.SessionFactory()
+    user_id = (await sio.server.get_session(sid)).get("user_id")
+    user = await get_user(user_id, session=session)
+    await exit_from_chats(sid, user)
+    await session.close()
 
 
 @sio.server.on("send message")
-async def send_message(sid, data: t.Dict[t.Literal["channel_id", "message"], t.Any]):
+async def send_message(
+    sid, data: typing.Dict[typing.Literal["channel_id", "message"], typing.Any]
+):
     channel_id = data.get("channel_id")
     message = data.get("message")
     user_id = (await sio.server.get_session(sid)).get("user_id")
